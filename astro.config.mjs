bd7c791922764c43a2511e55b6f4389639a442c2
Tyler <tyler.chetty7@gmail.com>
2025-12-05 03:53:54 -0500

    Update repository and add Github Pages workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://sp-05-grocery-list.github.io',
+});
